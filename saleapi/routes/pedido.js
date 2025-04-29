fbc96a6f1504ff0ac8469eba37c02149ab671530
victorcaetano2025 <victorcaetano2711@gmail>
2025-04-29 15:05:19 -0300

    working post from pedido.js

--- saleapi/routes/pedido.js
+++ saleapi/routes/pedido.js
@@ -13,7 +13,21 @@ try {
     console.error("erro ao buscar pedidos",error);
     res.status(500).json({erro: "salamaleiko nos pedidos" });
 }
+})
 
+router.post('/pedido', async(req,res)=>{
+    
+    const {codigo} = req.body;  
+
+try {
+    const query ="insert into pedido(codigo) values ($1) RETURNING *"; //returning * pode ser uma mensagem pois retorna o ultimo inserido
+    const values = [codigo]
+    const insercaopedido = await client.query(query,values)
+    res.status(201).json(res.rows[0])//a response que ira para o front ser a res.rows retornada pela query
+} catch (error) {
+    console.error("erro no post pedidos",error)
+    res.status(500).json({erro: "erro no post"})
+}
 
 })
 
