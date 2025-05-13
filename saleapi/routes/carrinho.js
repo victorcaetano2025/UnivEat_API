6bf082987c74b95f3c049f43d3888f6193515c13
victorcaetano2025 <victorcaetano2711@gmail>
2025-05-13 12:10:38 -0300

    +logic in carrinho

--- saleapi/routes/carrinho.js
+++ saleapi/routes/carrinho.js
@@ -16,16 +16,34 @@ router.get('/carrinho', async (req,res)=>{
 
 router.post('/carrinho', async(req,res)=>{
     
-    const {codigo} = req.body;  
+    const {pedido,produto,quantidade} = req.body;  
 
     try {
-        const query ="insert into carrinho(codigo) values ($1) RETURNING *"; //returning * pode ser uma mensagem pois retorna o ultimo inserido
-        const values = [codigo]
+        const query ="insert into carrinho(pedido,produto,quantidade_pedido) values ($1,$2,$3) RETURNING *"; //returning * pode ser uma mensagem pois retorna o ultimo inserido
+        const values = [pedido,produto,quantidade]
         const insercaocarrinho = await client.query(query,values)
         res.status(201).json(insercaocarrinho.rows[0]);//a response que ira para o front ser a res.rows retornada pela query
     } catch (error) {
         console.error("erro no post carrinho",error)
         res.status(500).json({erro: "erro no post"})
+    }
+
+})
+
+router.put('/carrinho', async(req,res)=>{
+    const{pedido,produto,quantidade} = req.body;
+    try {
+        const query= "update carrinho set quantidade_pedido = $3 where pedido = $1 and produto = $2 RETURNING *";
+        const values = [pedido,produto,quantidade];
+        const atualizacarrinho = await client.query(query,values);
+
+        if (atualizacarrinho.rowCount === 0) {
+        return res.status(404).json({ mensagem: 'Item do carrinho não encontrado.' });}
+        res.status(200).json(atualizacarrinho.rows[0]);
+
+    } catch (error) {
+    console.error('Erro ao atualizar carrinho:', error);
+    res.status(500).json({ erro: 'Erro interno no servidor.' });
     }
 
 })
